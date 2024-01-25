af510b121a269340aea9eb3543c1dcf8a6ea9c56
Jonathan <byersj920@gmail.com>
2024-01-24 18:22:55 -0600

    Enums are created, but site doesn't load planets properly

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,26 @@
+package org.launchcode.enumerableplanets.data;
+
+public enum Planets {
+    MERCURY("Mercury"),
+    VENUS("Venus"),
+    EARTH("Earth"),
+    MARS("Mars"),
+    JUPITER("Jupiter"),
+    SATURN("Saturn"),
+    URANUS("Uranus"),
+    NEPTUNE("Neptune");
+
+    private String name;
+
+    Planets(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/org/launchcode/enumerableplanets/EnumerableplanetsApplicationTests.java
+++ src/test/java/org/launchcode/enumerableplanets/EnumerableplanetsApplicationTests.java
@@ -1,13 +1,26 @@
 package org.launchcode.enumerableplanets;
 
 import org.junit.jupiter.api.Test;
+import org.launchcode.enumerableplanets.data.Planets;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EnumerableplanetsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void enumsAreWorking(){
+		assertEquals(Planets.values().length, 8);
+	}
+
+	@Test
+	void enumsValueTest(){
+		assertEquals(Planets.EARTH.getName(), "Earth");
 	}
 
 }
